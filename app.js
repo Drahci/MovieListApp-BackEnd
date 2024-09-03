2dd17fc151f6726ae317775a50e5a4a4ffbebd74
Richard <richard.ricciard@gmail.com>
2024-09-02 22:15:02 -0300

    Remover chamada ao método sync e ajustar inicialização do servidor

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const sequelize = require("./config/database");
 const app = express();
 const movieRoutes = require("./routes/movieRoutes");
 
@@ -10,14 +10,6 @@ app.use("/api", movieRoutes);
 
 const PORT = process.env.PORT || 3002;
 
-sequelize
-  .sync()
-  .then(() => {
-    console.log("Banco de dados sincronizado");
-    app.listen(PORT, () => {
-      console.log(`Servidor rodando na porta ${PORT}`);
-    });
-  })
-  .catch((error) => {
-    console.error("Erro ao sincronizar o banco de dados:", error);
-  });
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
